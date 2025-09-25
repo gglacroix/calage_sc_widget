491cfc5a456f2f964d89bbb4b9103a812acaec25
alshim <gglacroix@outlook.fr>
2025-09-25 19:57:30 +0200

    ajout de la gestion du background, implémentation de la génération du fichier final

--- record_headless.sh
+++ record_headless.sh
@@ -2,9 +2,22 @@
 set -Eeuo pipefail
 
 # ----------- Paramètres -----------
-TRACK_ID="${1:-2113654521}"
-DUR="${2:-30}"
-OUT="${3:-out.mp4}"
+TRACK_URL="${1:-https://soundcloud.com/calage/exalk-hurt-feelings}"
+VIDEO_DURATION="${2:-30}"
+AUDIO_TC_IN="${3:-00:03:30}"
+AUDIO_DURATION="${4:-30}"
+FILE_OUT="${5:-out.mp4}"
+BACKGROUND_URL="${6:-https://www.youtube.com/watch?v=ywa7QQTjSkE}"
+BACKGROUND_DURATION=30
+BACKGROUND_TC_IN="${7:-00:38:35}"
+
+start_epoch=$(date -u -d "1970-01-01 ${BACKGROUND_TC_IN} UTC" +%s) || {
+  echo "timecode invalide: ${BACKGROUND_TC_IN}" >&2; exit 1;
+}
+end_epoch=$(( start_epoch + BACKGROUND_DURATION ))
+BACKGROUND_TC_OUT=$(date -u -d "@${end_epoch}" +%T)
+
+TRACK_ID=$(bin/yt-dlp --print "%(id)s" "$TRACK_URL")
 
 # Informations serveurs web
 HTML_FILE="sc_player.html"
@@ -55,18 +68,40 @@ CHROM_PID=$!
 # Laisse charger la page
 sleep 3
 
-# ----------- Enregistrement ffmpeg (X11 + Pulse monitor) -----------
+# ----------- Enregistrement ffmpeg (Widget) -----------
 ffmpeg -y \
   -video_size "$DISPLAY_WIDTH"x"$DISPLAY_HEIGHT" -framerate 30 \
   -f x11grab -draw_mouse 0 -i :99.0+0,0 \
-  -f pulse -thread_queue_size 1024 -i virt_sink.monitor \
-  -t "$DUR" \
+  -t "$VIDEO_DURATION" \
   -vf "crop=350:344:365:866" \
   -c:v libx264 -preset slow -crf 16 -pix_fmt yuv420p \
-  -c:a aac -b:a 192k "$OUT"
+  -c:a aac -b:a 192k "widget.mp4"
 
 
-echo "✅ Terminé : $OUT"
+# ----------- Téléchargement extrait audio -----------
+
+bin/yt-dlp -x --audio-format mp3 "$TRACK_URL" -o "audio.mp3"
+ffmpeg -ss "$AUDIO_TC_IN" -t "$AUDIO_DURATION" -i "audio.mp3" -acodec copy "audio_extract.mp3"
+
+# ----------- Téléchargement extrait vidéo background -----------
+
+bin/yt-dlp -f "bv*[ext=mp4]/bv*" \
+  --download-sections "*$BACKGROUND_TC_IN-$BACKGROUND_TC_OUT" \
+  -o "background.mp4" \
+  "$BACKGROUND_URL"
+
+# ----------- Création du fichier final -----------
+
+ffmpeg -i background.mp4 -i widget.mp4 -i audio_extract.mp3 \
+-filter_complex "\
+[0:v]scale=1080:1920:force_original_aspect_ratio=increase, \
+ crop=1080:1920,setsar=1,boxblur=20:1[bg]; \
+[1:v]scale=900:-2:force_original_aspect_ratio=decrease,setsar=1[fg]; \
+[bg][fg]overlay=(W-w)/2:(H-h)/2:shortest=1[v]" \
+-map "[v]" -map 2:a -c:v libx264 -preset medium -crf 20 -c:a aac -shortest "$FILE_OUT"
+
+echo "✅ Terminé : $FILE_OUT"
 
 rm -rf ~/.cache/chromium
+rm audio.mp3
 echo "✅ Supprime le cache pour les prochains exports"
